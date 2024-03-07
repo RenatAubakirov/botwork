6d245da875246c956ae65c2516279884c0c9e4a2
Renat Aubakirov <arenat98@mail.ru>
2024-03-07 23:23:43 +0700

    commit "button creation"

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import schedule
 from google.oauth2 import service_account
 from googleapiclient import discovery
 
-bot = telebot.TeleBot('7029317973:AAFoUffqtX66UOcGK-ye1B0D299LTl0ze4E')
+bot = telebot.TeleBot('7029317973:AAFoUffqtX66UOcGK-ye1B0D299LTl0ze4E') ## ЗДЕСЬ ВСТАВЛЯЕМ ТОКЕН БОТА
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -19,11 +19,15 @@ def start(message):
     btn2 = types.KeyboardButton("Работа")
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id, "👋 Привет! Я твой бот-помощник в аутстафе ТК95", reply_markup=markup)
+## Кнопки
+@bot.message_handler(func=lambda message: message.text == "Старт")
+def start_command(message):
+    bot.send_message(message.chat.id, "Выполняю команду start")
 
 @bot.message_handler(func=lambda message: message.text == "Работа")
 def check_calendar(message):
     job(message)
-
+# Количество дней просмотра календаря "7"
 def job(message):
     credentials = service_account.Credentials.from_service_account_file('credentials.json')
     service = discovery.build('calendar', 'v3', credentials=credentials)
@@ -34,6 +38,7 @@ def job(message):
 
     eventsResult = service.events().list(
         calendarId='7cf44d58482818e043c1b95b293016277bc379c15b68102f322f276ab657f8e3@group.calendar.google.com',
+        # Здесь вставляем id именно календаря который используется, находится в настройках редактируемого календаря в пункте интеграция календаря
         timeMin=now,
         timeMax=end_of_week,
         maxResults=100,
@@ -42,15 +47,20 @@ def job(message):
     ).execute()
 
     events = eventsResult.get('items', [])
-
+# Вывод
     if not events:
         bot.send_message(message.chat.id, 'На этой неделе нет событий в календаре.')
     else:
-        msg = '<b>События на этой неделе:</b>\n\n'
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
             summary = event['summary']
-            msg += f"<b>{summary}</b> - {start}\n"
-        bot.send_message(message.chat.id, msg, parse_mode='HTML')
+            msg = f"<b>{summary}</b> - {start}"
+            bot.send_message(message.chat.id, msg, parse_mode='HTML')
+
+# Изменение: Добавлен обработчик для кнопки "Поздороваться"
+@bot.message_handler(func=lambda message: message.text == "👋 Поздороваться")
+def greet(message):
+    bot.send_message(message.chat.id, "Добрый день. Я рад, что вы воспользовались мной. Если вы хотите посмотреть актуальные смены на ближайшую неделю, нажмите кнопку \"РАБОТА\".")
+
 
 bot.polling(none_stop=True)
--- readme1.txt
+++ readme1.txt
@@ -7,3 +7,9 @@
 Описание Bot API смотрите на этой странице: https://core.telegram.org/bots/api.
 
 worktk95rbot@worktk95.iam.gserviceaccount.com
+
+Ссылка на гугл календарь
+https://calendar.google.com/calendar/u/0/r/settings/calendar/N2NmNDRkNTg0ODI4MThlMDQzYzFiOTViMjkzMDE2Mjc3YmMzNzljMTViNjgxMDJmMzIyZjI3NmFiNjU3ZjhlM0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t
+
+API гугл календаря
+https://console.cloud.google.com/cloud-resource-manager?previousPage=%2Fapis%2Fenableflow%3Fapiid%3Dcalendar%26project%3Dworktk95&organizationId=0
