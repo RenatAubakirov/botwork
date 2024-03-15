177a4e056a417d0b4735d9739e56b81e1aaabb20
Renat Aubakirov <arenat98@mail.ru>
2024-03-15 23:09:35 +0700

    first commit

--- config.py
+++
@@ -1,7 +0,0 @@
-#Это конфиг файл config.py с данными для интеграционного взаимодейстия
-# -*- coding: utf-8-*-
-
-TOKEN = 'Свой токен бота'
-
-client_secret_calendar='/credentials.json' #указываем путь к скачанному Json
-
